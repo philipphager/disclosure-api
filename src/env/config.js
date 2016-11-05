6405861cc8e5e2084653b2b80ccd6bd71a351ec3
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-05 11:39:00 +0100

    Add support for mongolab

--- src/env/config.js
+++ src/env/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        url: process.env.MONGODB_URI || 'mongodb://localhost/test'
+        url: process.env.MONGOLAB_URI || 'mongodb://localhost/test'
     },
     server: {
         port: process.env.SERVER_PORT || 3000
