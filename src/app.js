3b26e7fa1e1cf8f8c6689728cc2a3448ba9c1977
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2016-11-09 02:23:16 +0100

    Add promises

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const router = require('./router');
 
 // Database -------------------------------------------------------------------
 mongoose.Promise = global.Promise;
-mongoose.connect(config.db.url, (err) => {
-    if (err) {
-        return console.log(`Error while connecting to mongodb with ${config.db.url}`);
-    }
+mongoose.connect(config.db.url)
+.then(() => {
     console.log(`Successfully connected with ${config.db.url}`);
+})
+.catch((err) => {
+    console.log(`Error while connecting to mongodb with ${config.db.url}`);
 });
 
 // Express App ----------------------------------------------------------------
@@ -22,8 +23,7 @@ app.use(bodyParser.json());
 app.use(router());
 
 // Start Server ---------------------------------------------------------------
-app.listen(config.server.port, (err) => {
-    if (!err) {
-        console.log(`Disclosure listening on port ${config.server.port}`);
-    }
+app.listen(config.server.port)
+.then(() => {
+    console.log(`Disclosure listening on port ${config.server.port}`);
 });
