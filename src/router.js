fa9a634311e3cd8c565ebb329b2456a6851b7f58
Philipp Hager <philipp.hager@fh-duesseldorf.de>
2017-02-27 21:24:45 +0100

    Remove features

--- src/router.js
+++ src/router.js
@@ -2,14 +2,10 @@
 const express = require('express');
 const homeController = require('./controller/home.js');
 const libraryController = require('./controller/library.js');
-const featureController = require('./controller/feature.js');
-const libraryFeatureController = require('./controller/library-feature.js');
 
 module.exports = () => {
     const router = express.Router();
     router.use('/', homeController());
     router.use('/libraries', libraryController());
-    router.use('/features', featureController());
-    router.use('/libraryFeatures', libraryFeatureController()); // TODO: rename
     return router;
 };
